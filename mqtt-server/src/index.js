ee975ecb844a5e127652ed3b9d6d3f741cc4d50f
Andres Gazui <gazuiandres@gmail.com>
2023-03-29 14:24:57 -0400

    [add] mqtt server configuration

--- mqtt-server/src/index.js
+++ mqtt-server/src/index.js
@@ -5,8 +5,43 @@ const port = 9000
 
 const log = console.log
 
+const clients = new Map()
+
 aedes.on('client', function (client) {
-    log(chalk.green(`[client-connected]: ${client.id}`))
+  log(chalk.green(`[client-connected]: ${client.id}`))
+})
+
+aedes.on('clientDisconnect', function (client) {
+  log(chalk.blue(`[client-disconnected]: ${client.id}`))
+  const agent = clients.get(client.id)
+
+  if (agent) {
+    aedes.publish({
+      topic: 'Agent/Disconnected',
+      payload: JSON.stringify({
+        agent: {
+          uuid: agent.uuid,
+          connected: false,
+          name: agent.name
+        }
+      })
+    })
+  }
+
+  clients.delete(client.id)
+})
+
+aedes.on('publish', function (packet, client) {
+  if (client) {
+    const payload = packet.payload
+
+    if (packet.topic === 'Agent/Connected') {
+      if (!clients.get(client.id)) {
+        clients.set(client.id, JSON.parse(payload))
+      }
+    }
+
+  }
 })
 
 server.listen(port, function () {
