85b2301946c86c7dfea935b7b30cc74974ff0c07
Andres Gazui <gazuiandres@gmail.com>
2023-03-31 17:15:56 -0400

    [setup] web client

---
+++ web-client/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h1 className="text-3xl text-blue-500 font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default Home
